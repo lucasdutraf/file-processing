aad481d8f2d550ccc8bed0e9c5d92ee57d1ecaf3
Lucas Dutra <ldutra98@gmail.com>
2025-02-04 16:05:11 -0300

    Create file sending endpoint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
@@ -8,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
